f57e4ccd4114ed580a354136455fbac58f8fb922
savan Kumar <121648350+ShivSaini011@users.noreply.github.com>
2026-01-21 17:52:30 +0530

    recreating

---
+++ docs/_next/static/uLps3kLAFYVt5pE2bAmPH/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/ishan-learning-page//_next/:path+",
+        "destination": "/ishan-learning-page/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
